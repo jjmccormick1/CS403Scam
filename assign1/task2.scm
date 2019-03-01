b8ad03469f0899bda4ce72ae59e9e0ab3cdb8bc5
Jeremy McCormick <jjmccormick@crimson.ua.edu>
2019-02-28 20:44:21 -0600

    Working on triangle

---
+++ assign1/task2.scm
@@ -0,0 +1,31 @@
+(define (main)
+        (setPort (open (getElement ScamArgs 1) 'read))
+        (define threshold (readExpr))
+        (define r (readExpr))
+        (define s (readExpr))
+        (println "((mandelbrot " threshold ") " r " " s ") is " ((mandelbrot threshold) r s ))
+        )
+
+
+(define (mandelbrot thresh)
+  (lambda (x y)
+    (define (iter r s t)
+      (cond
+        ((= t thresh) (0))
+        (else 
+            (cond
+                ((> 4 (+ (* r r) (* s s))) (iter 
+                                                (+ x (- (* r r) (* s s)))
+                                                (+ y (* r s 2))
+                                                (+ 1 t)
+                    )
+                )
+                (else t)
+                )
+            )
+         )
+      )
+    ; call with default values 0.0 for r and 0.0 for s
+    (iter 0.0 0.0 0)
+)
+)
--- assign1/task4.scm
+++ assign1/task4.scm
@@ -2,10 +2,58 @@
         (setPort (open (getElement ScamArgs 1) 'read))
         (define arg1 (readExpr))
         (define arg2 (readExpr))
-        (define numLevels (readExpr))
-        (println (crazyTriangle arg1 arg2))
+        (define levels (readExpr))
+        (inspect ((crazyTriangle arg1 arg2) levels  ))
         )
 
-(define (crazyTriangle arg1 arg2)
-        (println arg1 " "  arg2)
+(define (crazyTriangle left right)
+  (lambda (numLevels)  
+        ;Finds the value of the number to print
+        (define (getNum numLevel numColumn)
+            (cond
+            ((= numColumns 0) left)  ; If at far left side, return left    
+            ((= levels column) right) ;if at far right side, return right
+            (else                    ; otherwise, recur
+                (+ 
+                    (getNum (- numLevel 1) (- numColumn 1)) ;up and over
+                    (helper (- numLevel 1) numColumn)        ; above
+                )
+                )
+            )
         )
+        
+        (define (padSpaces numSpaces)
+            (cond
+                ( (> numSpaces 0) 
+                    ( (print " ") 
+                      (padSpaces (- numSpaces 1))
+                    )
+                )
+            ) 
+        )
+        
+        (define (printLevel numLevel)
+            (cond
+                ( (< numLevel numLevels)
+                    (
+                        (padSpaces (- numLevels numLevel)) ;pad by which level were on
+                        (define (printNum num)
+                            (cond 
+                                (  (<= num numLevel )
+                                    (
+                                        (print (getNum numLevel num))
+                                        (printNum (+ num 1))
+                                    )
+                                )   
+                            )
+                        )
+                        (printNum 0)
+                        (println)
+                        (printLevel (+ level 1))
+                    )
+                )
+            )
+        )
+        (printLevel 0)
+    )
+)
