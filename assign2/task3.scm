1fa8bd4fb3789753acbbf213f0d9fdb91a8477a8
Unknown <jjmccormick@crimson.ua.edu>
2019-03-17 14:38:14 -0500

    Start task 3

---
+++ assign2/task3.scm
@@ -0,0 +1,11 @@
+ (define (main)
+        (setPort (open (getElement ScamArgs 1) 'read))
+        (define env this)
+        (define (iter expr)
+            (if (not (eof?)) (begin (eval expr env) (iter (readExpr))))
+        )
+        (iter (readExpr))
+        (env)
+)
+
+(define (Stack 
