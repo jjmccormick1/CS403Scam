6a712e06a0e8996bcb4734f9b84f950649caf84d
jj <jjmccormick@crimson.ua.edu>
2019-04-02 08:14:52 -0500

    Added notes from class

---
+++ 3-28CH3.scm
@@ -0,0 +1,71 @@
+;; 1/1 -1/3 + 1/5 - 1/7 + 1/9 .... should sum to pi/4
+(define ones (cons-stream 1 ones))
+(define twos (cons-stream 2 ones))
+(define alt-ones 
+    (cons-stream 1 (cons-stream -1 alt-ones))
+)
+(define odds (cons-stream 1 (combine-streams + twos odds)))
+
+(define (// a b) (/ (real a) (real b)))
+(define (sdisplay z n)
+    (cond
+        ((= n 0) (println "..."))
+        (else 
+            (print (stream-car z) " ")
+            (sdisplay (stream-cdr z) (- n 1))
+        )
+    )
+)
+(define (sref z n)
+    (cond
+        ((= n 0) (stream-car z))
+        (else 
+            (sref (stream-cdr z) (- n 1))
+        )
+    )
+)
+(define (combine-streams op s t)
+    (cons-stream
+        (op (stream-car s) (stream-car t))
+        (combine-streams op (stream-cdr s) (stream-cdr t))
+    )    
+)
+(define (ps s) 
+    (cons-stream (stream-car s) (combine-streams + (stream-cdr s) (ps s)))
+)
+
+(define (st s)
+    (define s0 (sref s 0))
+    (define s1 (sref s 1))
+    (define s2 (sref s 2))
+    (cons-stream 
+        (/ (- (* s2 s0) (* s1 s1)) (+ s2 (- (* 2 s1)) s0))
+        (st (stream-cdr s))
+    )    
+)
+(define (tt xform s)
+    (cons-stream s (tt xform (xform s)))
+)
+
+(define (stream-map f s)
+    (cons-stream 
+        (f (stream-car s))
+        (stream-map f (stream-cdr s))
+    )   
+)
+
+(define (add-streams a b)
+    (combine-streams + a b)
+)
+(define pi-stream (combine-streams // alt-ones odds))
+(define pi/4 (ps pi-stream))
+(define accpi/4 (st (ps pi/4)))
+(define superaccpi/4 (stream-map stream-car (tt st (ps pi/4))))
+
+(define wholes (cons-stream 0 (add-streams ones wholes)))
+
+(define (pairs s t)
+    (cons-stream
+        (list (stream-car s) (stream-car t))
+        
+)
